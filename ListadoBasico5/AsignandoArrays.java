7e1793cb58e32dfa71b7649ab554224e0b655091
Miguel Ángel López Moyano <i02lomom@gmail.com>
2015-12-07 14:09:06 +0100

    Update AsignandoArrays.java

--- ListadoBasico5/AsignandoArrays.java
+++ ListadoBasico5/AsignandoArrays.java
@@ -1,19 +1,19 @@
 package ListadoBasico5;
 import utiles.*;
 /**
-* Define la clase AsignandoArrays tal y como muestra la captura. Indica en un
-* comentario lo que almacenaría la referencia a1 tras modificarse a2. Añade también los
-* siguientes comentarios en las líneas correspondientes:
-*
-* a. se referencia al mismo objeto
-* b. modificación de los elementos
-* c. declaración de un array
-* d. se muestran los elementos del objeto array
-* e. declaración, construcción e inicialización de un array
-* 
-* @author Miguel Angel López Moyano
-* @version 1.0
-*/
+ * Define la clase AsignandoArrays tal y como muestra la captura. Indica en un
+ * comentario lo que almacenaría la referencia a1 tras modificarse a2. Añade también los
+ * siguientes comentarios en las líneas correspondientes:
+ *
+ * a. se referencia al mismo objeto
+ * b. modificación de los elementos
+ * c. declaración de un array
+ * d. se muestran los elementos del objeto array
+ * e. declaración, construcción e inicialización de un array
+ * 
+ * @author Miguel Angel López Moyano
+ * @version 1.0
+ */
 public class AsignandoArrays{
 	public static void main(String[] args){
 		int [] a1 = { 3, 5, 7, 11, 13 } ;	//declaración, construcción e inicialización de un array
